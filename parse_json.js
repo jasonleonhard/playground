4c3ee66fc1d2424065f77a10dec0f9f63a0e01ab
jasonleonhard <leonhard.jason@gmail.com>
2018-11-05 11:34:48 -0800

    add some data and update some info

---
+++ parse_json.js
@@ -0,0 +1,734 @@
+// var json = '{"result":true, "count":42}';
+var json = {
+  "faceAnnotations": [
+    {
+      "boundingPoly": {
+        "vertices": [
+          {
+            "x": 21,
+            "y": 25
+          },
+          {
+            "x": 107,
+            "y": 25
+          },
+          {
+            "x": 107,
+            "y": 125
+          },
+          {
+            "x": 21,
+            "y": 125
+          }
+        ]
+      },
+      "fdBoundingPoly": {
+        "vertices": [
+          {
+            "x": 26,
+            "y": 51
+          },
+          {
+            "x": 101,
+            "y": 51
+          },
+          {
+            "x": 101,
+            "y": 126
+          },
+          {
+            "x": 26,
+            "y": 126
+          }
+        ]
+      },
+      "landmarks": [
+        {
+          "type": "LEFT_EYE",
+          "position": {
+            "x": 47.242805,
+            "y": 77.15183,
+            "z": -0.0005075807
+          }
+        },
+        {
+          "type": "RIGHT_EYE",
+          "position": {
+            "x": 75.70905,
+            "y": 71.759125,
+            "z": -1.8725126
+          }
+        },
+        {
+          "type": "LEFT_OF_LEFT_EYEBROW",
+          "position": {
+            "x": 39.21265,
+            "y": 72.77391,
+            "z": 2.3179986
+          }
+        },
+        {
+          "type": "RIGHT_OF_LEFT_EYEBROW",
+          "position": {
+            "x": 54.33633,
+            "y": 70.465805,
+            "z": -6.3834004
+          }
+        },
+        {
+          "type": "LEFT_OF_RIGHT_EYEBROW",
+          "position": {
+            "x": 66.95093,
+            "y": 68.02403,
+            "z": -7.2017937
+          }
+        },
+        {
+          "type": "RIGHT_OF_RIGHT_EYEBROW",
+          "position": {
+            "x": 82.97351,
+            "y": 65.12668,
+            "z": -0.34692165
+          }
+        },
+        {
+          "type": "MIDPOINT_BETWEEN_EYES",
+          "position": {
+            "x": 61.250813,
+            "y": 74.484924,
+            "z": -6.4929447
+          }
+        },
+        {
+          "type": "NOSE_TIP",
+          "position": {
+            "x": 63.78027,
+            "y": 90.063354,
+            "z": -11.88685
+          }
+        },
+        {
+          "type": "UPPER_LIP",
+          "position": {
+            "x": 65.68271,
+            "y": 98.050255,
+            "z": -4.784104
+          }
+        },
+        {
+          "type": "LOWER_LIP",
+          "position": {
+            "x": 67.39002,
+            "y": 106.43845,
+            "z": -1.7297082
+          }
+        },
+        {
+          "type": "MOUTH_LEFT",
+          "position": {
+            "x": 54.34522,
+            "y": 102.22206,
+            "z": 3.8773072
+          }
+        },
+        {
+          "type": "MOUTH_RIGHT",
+          "position": {
+            "x": 79.18009,
+            "y": 97.75505,
+            "z": 2.551066
+          }
+        },
+        {
+          "type": "MOUTH_CENTER",
+          "position": {
+            "x": 66.48111,
+            "y": 102.21565,
+            "z": -2.2934906
+          }
+        },
+        {
+          "type": "NOSE_BOTTOM_RIGHT",
+          "position": {
+            "x": 72.676575,
+            "y": 88.56131,
+            "z": -2.0933394
+          }
+        },
+        {
+          "type": "NOSE_BOTTOM_LEFT",
+          "position": {
+            "x": 55.88419,
+            "y": 91.78914,
+            "z": -0.9782795
+          }
+        },
+        {
+          "type": "NOSE_BOTTOM_CENTER",
+          "position": {
+            "x": 64.73013,
+            "y": 93.07673,
+            "z": -5.5620008
+          }
+        },
+        {
+          "type": "LEFT_EYE_TOP_BOUNDARY",
+          "position": {
+            "x": 48.771873,
+            "y": 75.35135,
+            "z": -2.1631572
+          }
+        },
+        {
+          "type": "LEFT_EYE_RIGHT_CORNER",
+          "position": {
+            "x": 54.31641,
+            "y": 76.0364,
+            "z": -0.39629003
+          }
+        },
+        {
+          "type": "LEFT_EYE_BOTTOM_BOUNDARY",
+          "position": {
+            "x": 47.89336,
+            "y": 78.64552,
+            "z": -0.10600522
+          }
+        },
+        {
+          "type": "LEFT_EYE_LEFT_CORNER",
+          "position": {
+            "x": 43.94233,
+            "y": 77.6196,
+            "z": 2.6120675
+          }
+        },
+        {
+          "type": "LEFT_EYE_PUPIL",
+          "position": {
+            "x": 48.749672,
+            "y": 76.95586,
+            "z": -0.91371554
+          }
+        },
+        {
+          "type": "RIGHT_EYE_TOP_BOUNDARY",
+          "position": {
+            "x": 73.922485,
+            "y": 70.856094,
+            "z": -3.8327546
+          }
+        },
+        {
+          "type": "RIGHT_EYE_RIGHT_CORNER",
+          "position": {
+            "x": 79.50951,
+            "y": 71.16325,
+            "z": 0.17903
+          }
+        },
+        {
+          "type": "RIGHT_EYE_BOTTOM_BOUNDARY",
+          "position": {
+            "x": 75.86638,
+            "y": 73.44258,
+            "z": -1.844853
+          }
+        },
+        {
+          "type": "RIGHT_EYE_LEFT_CORNER",
+          "position": {
+            "x": 69.75599,
+            "y": 73.40739,
+            "z": -1.3715571
+          }
+        },
+        {
+          "type": "RIGHT_EYE_PUPIL",
+          "position": {
+            "x": 74.53043,
+            "y": 72.35263,
+            "z": -2.6577044
+          }
+        },
+        {
+          "type": "LEFT_EYEBROW_UPPER_MIDPOINT",
+          "position": {
+            "x": 46.347786,
+            "y": 68.52168,
+            "z": -4.418951
+          }
+        },
+        {
+          "type": "RIGHT_EYEBROW_UPPER_MIDPOINT",
+          "position": {
+            "x": 73.58922,
+            "y": 63.658363,
+            "z": -6.236266
+          }
+        },
+        {
+          "type": "LEFT_EAR_TRAGION",
+          "position": {
+            "x": 36.781796,
+            "y": 87.236374,
+            "z": 33.884384
+          }
+        },
+        {
+          "type": "RIGHT_EAR_TRAGION",
+          "position": {
+            "x": 93.853615,
+            "y": 77.030304,
+            "z": 30.07593
+          }
+        },
+        {
+          "type": "FOREHEAD_GLABELLA",
+          "position": {
+            "x": 60.335793,
+            "y": 69.17015,
+            "z": -7.8634863
+          }
+        },
+        {
+          "type": "CHIN_GNATHION",
+          "position": {
+            "x": 69.61089,
+            "y": 117.0538,
+            "z": 3.200559
+          }
+        },
+        {
+          "type": "CHIN_LEFT_GONION",
+          "position": {
+            "x": 42.160717,
+            "y": 104.09433,
+            "z": 25.619759
+          }
+        },
+        {
+          "type": "CHIN_RIGHT_GONION",
+          "position": {
+            "x": 93.61698,
+            "y": 94.925644,
+            "z": 22.193802
+          }
+        }
+      ],
+      "rollAngle": -10.639548,
+      "panAngle": -3.7792125,
+      "tiltAngle": -7.684528,
+      "detectionConfidence": 0.7567631,
+      "landmarkingConfidence": 0.4659987,
+      "joyLikelihood": "VERY_LIKELY",
+      "sorrowLikelihood": "VERY_UNLIKELY",
+      "angerLikelihood": "VERY_UNLIKELY",
+      "surpriseLikelihood": "VERY_UNLIKELY",
+      "underExposedLikelihood": "VERY_UNLIKELY",
+      "blurredLikelihood": "VERY_UNLIKELY",
+      "headwearLikelihood": "VERY_UNLIKELY"
+    }
+  ],
+  "labelAnnotations": [
+    {
+      "mid": "/m/04yx4",
+      "description": "man",
+      "score": 0.9406871,
+      "topicality": 0.9406871
+    },
+    {
+      "mid": "/m/01g317",
+      "description": "person",
+      "score": 0.93463147,
+      "topicality": 0.93463147
+    },
+    {
+      "mid": "/m/0f9swq",
+      "description": "chin",
+      "score": 0.77621704,
+      "topicality": 0.77621704
+    },
+    {
+      "mid": "/m/025kyy",
+      "description": "forehead",
+      "score": 0.7317305,
+      "topicality": 0.7317305
+    },
+    {
+      "mid": "/m/019nj4",
+      "description": "smile",
+      "score": 0.6991037,
+      "topicality": 0.6991037
+    },
+    {
+      "mid": "/m/027v9wk",
+      "description": "elder",
+      "score": 0.68922406,
+      "topicality": 0.68922406
+    }
+  ],
+  "safeSearchAnnotation": {
+    "adult": "VERY_UNLIKELY",
+    "spoof": "UNLIKELY",
+    "medical": "VERY_UNLIKELY",
+    "violence": "VERY_UNLIKELY",
+    "racy": "VERY_UNLIKELY"
+  },
+  "imagePropertiesAnnotation": {
+    "dominantColors": {
+      "colors": [
+        {
+          "color": {
+            "red": 238,
+            "green": 239,
+            "blue": 246
+          },
+          "score": 0.20201719,
+          "pixelFraction": 0.22170858
+        },
+        {
+          "color": {
+            "red": 142,
+            "green": 163,
+            "blue": 89
+          },
+          "score": 0.115058415,
+          "pixelFraction": 0.06213018
+        },
+        {
+          "color": {
+            "red": 228,
+            "green": 240,
+            "blue": 176
+          },
+          "score": 0.08428445,
+          "pixelFraction": 0.13452293
+        },
+        {
+          "color": {
+            "red": 92,
+            "green": 132,
+            "blue": 57
+          },
+          "score": 0.06979501,
+          "pixelFraction": 0.015162722
+        },
+        {
+          "color": {
+            "red": 69,
+            "green": 81,
+            "blue": 98
+          },
+          "score": 0.026228588,
+          "pixelFraction": 0.015994823
+        },
+        {
+          "color": {
+            "red": 109,
+            "green": 73,
+            "blue": 50
+          },
+          "score": 0.010195639,
+          "pixelFraction": 0.013960799
+        },
+        {
+          "color": {
+            "red": 111,
+            "green": 131,
+            "blue": 58
+          },
+          "score": 0.06864087,
+          "pixelFraction": 0.01627219
+        },
+        {
+          "color": {
+            "red": 185,
+            "green": 204,
+            "blue": 135
+          },
+          "score": 0.06073229,
+          "pixelFraction": 0.15134986
+        },
+        {
+          "color": {
+            "red": 124,
+            "green": 163,
+            "blue": 84
+          },
+          "score": 0.05905855,
+          "pixelFraction": 0.026164941
+        },
+        {
+          "color": {
+            "red": 229,
+            "green": 241,
+            "blue": 195
+          },
+          "score": 0.038315754,
+          "pixelFraction": 0.018121302
+        }
+      ]
+    }
+  },
+  "cropHintsAnnotation": {
+    "cropHints": [
+      {
+        "boundingPoly": {
+          "vertices": [
+            {},
+            {
+              "x": 155
+            },
+            {
+              "x": 155,
+              "y": 194
+            },
+            {
+              "y": 194
+            }
+          ]
+        },
+        "confidence": 1,
+        "importanceFraction": 1.54
+      },
+      {
+        "boundingPoly": {
+          "vertices": [
+            {},
+            {
+              "x": 194
+            },
+            {
+              "x": 194,
+              "y": 194
+            },
+            {
+              "y": 194
+            }
+          ]
+        },
+        "confidence": 1,
+        "importanceFraction": 1.5699999
+      },
+      {
+        "boundingPoly": {
+          "vertices": [
+            {},
+            {
+              "x": 194
+            },
+            {
+              "x": 194,
+              "y": 164
+            },
+            {
+              "y": 164
+            }
+          ]
+        },
+        "confidence": 1,
+        "importanceFraction": 1.54
+      }
+    ]
+  },
+  "webDetection": {
+    "webEntities": [
+      {
+        "entityId": "/m/0lp09",
+        "score": 0.5773,
+        "description": "man page"
+      },
+      {
+        "entityId": "/m/0s9chxk",
+        "score": 0.5652
+      },
+      {
+        "entityId": "/m/07scx",
+        "score": 0.5544,
+        "description": "Unix"
+      },
+      {
+        "entityId": "/m/020s1",
+        "score": 0.5108,
+        "description": "Computer file"
+      },
+      {
+        "entityId": "/m/02cjl",
+        "score": 0.4214,
+        "description": "Data structure"
+      },
+      {
+        "entityId": "/m/0g5t5qn",
+        "score": 0.4176,
+        "description": "Min-max heap"
+      },
+      {
+        "entityId": "/m/0clkm",
+        "score": 0.4003,
+        "description": "grep"
+      },
+      {
+        "entityId": "/m/03y39",
+        "score": 0.3903,
+        "description": "Information"
+      },
+      {
+        "entityId": "/m/098_xr",
+        "score": 0.3549,
+        "description": "Error message"
+      },
+      {
+        "entityId": "/m/026sq",
+        "score": 0.3508,
+        "description": "Data"
+      },
+      {
+        "entityId": "/m/01p2sd",
+        "score": 0.346,
+        "description": "Stack"
+      },
+      {
+        "entityId": "/m/0j5f0",
+        "score": 0.3339,
+        "description": "Binary heap"
+      },
+      {
+        "entityId": "/m/01zbkc",
+        "score": 0.3287,
+        "description": "Filename"
+      },
+      {
+        "entityId": "/m/01fk53",
+        "score": 0.3263,
+        "description": "Command"
+      },
+      {
+        "entityId": "/m/04g0kcw",
+        "score": 0.3032,
+        "description": "GitHub"
+      }
+    ],
+    "fullMatchingImages": [
+      {
+        "url": "https://avatars1.githubusercontent.com/u/2337379?s=400&amp;v=4"
+      },
+      {
+        "url": "https://avatars2.githubusercontent.com/u/2337379?s=88&amp;v=4"
+      }
+    ],
+    "partialMatchingImages": [
+      {
+        "url": "https://i.stack.imgur.com/mU0bj.png?s=32&amp;g=1"
+      }
+    ],
+    "pagesWithMatchingImages": [
+      {
+        "url": "https://github.com/robbyrussell/oh-my-zsh/issues/1898",
+        "pageTitle": "fork failed: resource temporarily unavailable · Issue #1898 ... - GitHub",
+        "fullMatchingImages": [
+          {
+            "url": "https://avatars2.githubusercontent.com/u/2337379?s=88&amp;v=4"
+          }
+        ]
+      },
+      {
+        "url": "https://stackoverflow.com/questions/10212192/how-to-grep-for-a-filename-instead-of-the-contents-of-a-file/39450888",
+        "pageTitle": "How to &amp;quot;grep&amp;quot; for a filename instead of the contents of a file? - Stack ...",
+        "partialMatchingImages": [
+          {
+            "url": "https://i.stack.imgur.com/mU0bj.png?s=32&amp;g=1"
+          }
+        ]
+      },
+      {
+        "url": "https://stackoverflow.com/questions/2501457/what-do-i-use-for-a-max-heap-implementation-in-python",
+        "pageTitle": "data structures - What do I use for a max-heap implementation in ...",
+        "partialMatchingImages": [
+          {
+            "url": "https://i.stack.imgur.com/mU0bj.png?s=32&amp;g=1"
+          }
+        ]
+      },
+      {
+        "url": "https://github.com/nteract/hydrogen/issues/894",
+        "pageTitle": "Insufficient contrast when running code with error · Issue #894 ...",
+        "fullMatchingImages": [
+          {
+            "url": "https://avatars2.githubusercontent.com/u/2337379?s=88&amp;v=4"
+          }
+        ]
+      },
+      {
+        "url": "https://unix.stackexchange.com/questions/9501/how-to-test-what-shell-i-am-using-in-a-terminal",
+        "pageTitle": "How to test what shell I am using in a terminal? - Unix &amp;amp; Linux Stack ...",
+        "partialMatchingImages": [
+          {
+            "url": "https://i.stack.imgur.com/mU0bj.png?s=32&amp;g=1"
+          }
+        ]
+      }
+    ],
+    "visuallySimilarImages": [
+      {
+        "url": "https://rogersbh.org/application/files/2314/9602/3339/Smith.jpg"
+      },
+      {
+        "url": "https://cpcwwwa.files.wordpress.com/2014/09/steve-e1412054045291.jpg?w=300&amp;h=300"
+      },
+      {
+        "url": "https://wdef.com/wp-content/uploads/2018/02/Jim-Reynolds.png"
+      },
+      {
+        "url": "https://johnpslattery.files.wordpress.com/2015/01/john-slattery-headshot.jpg?w=324&amp;h=433"
+      },
+      {
+        "url": "http://www.franoi.com/wp-content/uploads/2018/03/Caira-e1521077375975.jpeg"
+      },
+      {
+        "url": "https://image.shutterstock.com/image-photo/casual-man-smiling-camera-on-260nw-257070961.jpg"
+      },
+      {
+        "url": "http://www.ocfchurch.com/filerequest/3099"
+      },
+      {
+        "url": "https://image.shutterstock.com/image-photo/indian-man-park-260nw-368683475.jpg"
+      },
+      {
+        "url": "http://static1.squarespace.com/static/5742da5ce3214003461529ef/t/5811d100414fb5b9a084eed0/1477562624323/Arjan+Visser+300sq.jpg?format=300w"
+      },
+      {
+        "url": "https://static1.squarespace.com/static/599dc3dcf9a61e30f52dbff3/t/5a985c8671c10b4c31446d6f/1526426133537/Dr.+Kevin+Hyde%2C+psychologist+treating+anxiety++Palm+Harbor%2C+Florida.?format=300w"
+      }
+    ],
+    "bestGuessLabels": [
+      {
+        "label": "man"
+      }
+    ]
+  }
+}
+
+
+// obj = JSON.parse(json);
+
+// console.log(JSON.stringify({ json }));
+// var str = JSON.stringify({ json })
+
+// console.log(obj.count);
+// expected output: 42
+
+// console.log(obj.result);
+// expected output: true
+
+
+
+// imagePropertiesAnnotation.cropHintsAnnotation.webDetection.webEntities
+// console.log(json.faceAnnotations);
+// console.log(json.webDetection.webEntities);
+
+console.log(json.webDetection.webEntities.map(e => e.description));
+
+
+
+// nodemon ./parse_json.js localhost 8080
